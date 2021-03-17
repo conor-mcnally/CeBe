e17f88a4c8cec60b2ec7799aa1bc3a8402d11909
conor-mcnally <cmcn@gmx.co.uk>
2021-03-17 15:46:27 +0000

    Detect swear words from mic input

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import twitch
 import speech_recognition as sr
 import pyttsx3
+import csv
 
 # Twitch Variables
-clientId = ''
-clientSecret = ''
+clientId = 'gz3pppp9elbk6smuodu5pxeswaox7p'
+clientSecret = 'wc40p5dwvcoenypeymop7mla17l0pf'
 
 # Speech variables
 r = sr.Recognizer()
@@ -15,16 +16,24 @@ def connect_to_twitch():
     print(helix.user('mcnaldo69').display_name)
 
 
-def convert_speech_to_text(command):
+def convert_text_to_speech(command):
     # Initialise the engine
     engine = pyttsx3.init()
     engine.say(command)
     engine.runAndWait()
 
 
+def swear_words():
+    with open('swearWords.csv') as file:
+        csv_reader = csv.reader(file, delimiter=',')
+        for entry in csv_reader:
+            return entry
+
+
 if __name__ == '__main__':
     connect_to_twitch()
-    while True:
+    swearWords = swear_words()
+    while 1:
         try:
             with sr.Microphone() as source2:
                 r.adjust_for_ambient_noise(source2, duration=0.2)
@@ -35,10 +44,14 @@ if __name__ == '__main__':
                 MyText = MyText.lower()
 
                 print("Did you say " + MyText)
-                convert_speech_to_text(MyText)
+                convert_text_to_speech(MyText)
+                for word in swearWords:
+                    if word == MyText:
+                        print("Bad word detected!")
+
+
         except sr.RequestError as e:
             print("could not request results; {0}".format(e))
 
         except sr.UnknownValueError:
             print("Unknown error occurred")
-
