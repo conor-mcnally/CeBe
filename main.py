2cf9d170679d5cce061111dd35f45c57bfd82b93
conor-mcnally <cmcn@gmx.co.uk>
2021-03-18 15:12:22 +0000

    Big func update
    Read movie file, convert to audio.
    Read audio file, convert to text.
    Read text file, detect profanity and play sound.
    Removed twitch functionality

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-import twitch
 import speech_recognition as sr
+from playsound import playsound
+import moviepy.editor as mp
 import pyttsx3
 import csv
 
-# Twitch Variables
-clientId = ''
-clientSecret = ''
-
 # Speech variables
 r = sr.Recognizer()
-
-
-def connect_to_twitch():
-    helix = twitch.Helix(clientId, clientSecret)
-    print(helix.user('mcnaldo69').display_name)
-
-
-def convert_text_to_speech(command):
-    # Initialise the engine
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
 
 
 def swear_words():
@@ -30,25 +15,49 @@ def swear_words():
             return entry
 
 
-if __name__ == '__main__':
-    connect_to_twitch()
+def convert_video_to_audio(clip):
+    clip = mp.VideoFileClip(clip)
+    clip.audio.write_audiofile(r"converted.wav")
+
+
+def convert_audio_to_text():
+    audio = sr.AudioFile("converted.wav")
+    with audio as source:
+        audio_file = r.record(source)
+    result = r.recognize_google(audio_file)
+    return result
+
+
+def detect_profanity_from_audioText(text):
     swearWords = swear_words()
+    for word in swearWords:
+        if word in text:
+            print("Profanity detected!: " + word)
+            playsound('/Users/cmcn2802/PycharmProjects/CeBe/beep.wav')
+#           Do something
+
+
+detect_profanity_from_audioText(convert_audio_to_text())
+
+
+def convert_text_to_speech(command):
+    # Initialise the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+# Driver code
+if __name__ == '__main__':
     while 1:
         try:
             with sr.Microphone() as source2:
                 r.adjust_for_ambient_noise(source2, duration=0.2)
-
                 audio2 = r.listen(source2)
-
                 MyText = r.recognize_google(audio2)
                 MyText = MyText.lower()
-
                 print("Did you say " + MyText)
                 convert_text_to_speech(MyText)
-                for word in swearWords:
-                    if word == MyText:
-                        print("Bad word detected!")
-
 
         except sr.RequestError as e:
             print("could not request results; {0}".format(e))
